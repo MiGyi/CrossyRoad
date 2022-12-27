bcc2232085140a86a1e44d84c316030b546e70a8
Minh Giang <94895248+MiGyi@users.noreply.github.com>
2022-12-27 11:08:38 +0700

    Reup

--- src/Const.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-const int screenWidth = 1400;
-const int screenHeight = 800;
--- src/TextureHepler.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-#include "Const.h"
-#include <string>
-
-void GetTexture(float width, float height, std::string filepath, Texture2D& texture);
